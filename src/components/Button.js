d9280addf190dcba640b33157c18c3e5fe062d29
Pato <nyatindopatrick@gmail.com>
2021-03-03 12:32:33 +0300

    style and size buttons & screen

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Button({ button }) {
-  return <input type="button" value={button} />;
+const styles = (color, size) => ({
+  backgroundColor: color,
+  width: size ? '50%' : '25%',
+});
+
+export default function Button({ button, wide = false, color = '#f5913e' }) {
+  return <input type="button" value={button} style={styles(color, wide)} />;
 }
 
 Button.propTypes = {
   button: PropTypes.string.isRequired,
+  wide: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import Button from './Button';
 
-const groupOne = ['AC', '+/-', '%', '÷'];
-const groupTwo = ['7', '8', '9', 'X'];
-const groupThree = ['4', '5', '6', '-'];
-const groupFour = ['1', '2', '3', '+'];
-const groupFive = ['0', '.', '='];
+const groupOne = [{ btn: 'AC', color: 'e0e0e0' }, { btn: '+/-', color: 'e0e0e0' }, { btn: '%', color: 'e0e0e0' }, { btn: '÷' }];
+const groupTwo = [{ btn: '7', color: 'e0e0e0' }, { btn: '8', color: 'e0e0e0' }, { btn: '9', color: 'e0e0e0' }, { btn: 'X' }];
+const groupThree = [{ btn: '4', color: 'e0e0e0' }, { btn: '5', color: 'e0e0e0' }, { btn: '6', color: 'e0e0e0' }, { btn: '-' }];
+const groupFour = [{ btn: '1', color: 'e0e0e0' }, { btn: '2', color: 'e0e0e0' }, { btn: '3', color: 'e0e0e0' }, { btn: '+' }];
+const groupFive = [{ btn: '0', color: 'e0e0e0', wide: true }, { btn: '.', color: 'e0e0e0' }, { btn: '=' }];
 
 export default function ButtonPanel() {
   return (
     <div className="button-panel">
       <div>
         {groupOne.map(item => (
-          <Button button={item} key={item} />
+          <Button button={item.btn} key={item.btn} wide={item.wide} color={item.color} />
         ))}
       </div>
       <div>
         {groupTwo.map(item => (
-          <Button button={item} key={item} />
+          <Button button={item.btn} key={item.btn} wide={item.wide} color={item.color} />
         ))}
       </div>
       <div>
         {groupThree.map(item => (
-          <Button button={item} key={item} />
+          <Button button={item.btn} key={item.btn} wide={item.wide} color={item.color} />
         ))}
       </div>
       <div>
         {groupFour.map(item => (
-          <Button button={item} key={item} />
+          <Button button={item.btn} key={item.btn} wide={item.wide} color={item.color} />
         ))}
       </div>
       <div>
         {groupFive.map(item => (
-          <Button button={item} key={item} />
+          <Button button={item.btn} key={item.btn} wide={item.wide} color={item.color} />
         ))}
       </div>
     </div>
